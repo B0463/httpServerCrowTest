35c73057d22907af16735df8b610d973b6ddc8a4
Henri Kim Pinheiro Rodrigues <hkimpr40@gmail.com>
2025-07-22 03:52:28 -0300

    add front loader logic on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,40 @@
 #include "crow.h"
+#include <filesystem>
+#include <fstream>
+#include <sstream>
 
-int main() {
+int main(int argc, char *argv[]) {
+    namespace fs = std::filesystem;
+
+    // catch exec path
+    fs::path execPath = argv[0];
+    fs::path execDir = execPath.parent_path();
+    if(execDir.empty()) {
+        execDir = fs::current_path();
+    }
+
+    // catch the front paths
+    fs::path pathIndex = fs::canonical(execDir / "../front/index.html");
+
+    // copy the content of front files
+    std::ifstream fileIndex(pathIndex);
+    if(!fileIndex.is_open()) {
+        return 1;
+    }
+    std::stringstream buffer;
+    buffer << fileIndex.rdbuf();
+    std::string contentIndex = buffer.str();
+    fileIndex.close();
+
+    // start crow app
     crow::SimpleApp app;
 
-    CROW_ROUTE(app, "/")([](){
-        return "Hello world";
+    // routes
+    CROW_ROUTE(app, "/")([contentIndex](){
+        return crow::response(contentIndex);
     });
+
+    // define loglevel and run
     app.loglevel(crow::LogLevel::Warning);
     app.port(8080).multithreaded().run();
 }
